2117b96d8c44e84ced4e8241d65f144c3e3f17ef
kevinkirkl3y <kevinmkirkley@gmail.com>
2021-01-08 09:37:02 -0800

    add additional details to Engineer/Machine

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System;
+
 
 namespace Factory.Models
 {
@@ -10,6 +12,11 @@ namespace Factory.Models
     }
     public int EngineerId { get; set; }
     public string EngineerName { get; set; }
+    public string EngineerContact { get; set; }
+    [DisplayName("Add Date")]
+    [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyy-MM-dd}")]
+    public DateTime HireDate { get; set; }
+
     public virtual ICollection<MachineEngineer> Machines { get; set; }
   }
 }
---
+++ Factory/Models/Machine.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Factory.Models
+{
+  public class Machine
+  {
+    public Machine()
+    {
+      this.Engineers = new HashSet<MachineEngineer>();
+    }
+    public int MachineId { get; set; }
+    public string MachineName { get; set; }
+    public string SerialNumber { get; set; }
+    [DisplayName("Add Date")]
+    [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyy-MM-dd}")]
+    public DateTime InstallDate { get; set; }
+    public virtual ICollection<MachineEngineer> Engineers { get; set; }
+  }
+}
