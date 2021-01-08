e6edfdc739226af2ee1bc923a6d8b1c7f56ce7cb
kevinkirkl3y <kevinmkirkley@gmail.com>
2021-01-08 15:33:08 -0800

    created view for Engineers

---
+++ Factory/Migrations/20210108210919_Initial.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Factory.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Engineers",
+                columns: table => new
+                {
+                    EngineerId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    EngineerName = table.Column<string>(nullable: true),
+                    EngineerContact = table.Column<string>(nullable: true),
+                    HireDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Engineers", x => x.EngineerId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Licenses",
+                columns: table => new
+                {
+                    LicenseId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    LicenseType = table.Column<string>(nullable: true),
+                    IssueDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Licenses", x => x.LicenseId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Machines",
+                columns: table => new
+                {
+                    MachineId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    MachineName = table.Column<string>(nullable: true),
+                    SerialNumber = table.Column<string>(nullable: true),
+                    InstallDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Machines", x => x.MachineId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LicenseEngineer",
+                columns: table => new
+                {
+                    LicenseEngineerId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    LicenseId = table.Column<int>(nullable: false),
+                    EngineerId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LicenseEngineer", x => x.LicenseEngineerId);
+                    table.ForeignKey(
+                        name: "FK_LicenseEngineer_Engineers_EngineerId",
+                        column: x => x.EngineerId,
+                        principalTable: "Engineers",
+                        principalColumn: "EngineerId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_LicenseEngineer_Licenses_LicenseId",
+                        column: x => x.LicenseId,
+                        principalTable: "Licenses",
+                        principalColumn: "LicenseId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MachineEngineer",
+                columns: table => new
+                {
+                    MachineEngineerId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    EngineerId = table.Column<int>(nullable: false),
+                    MachineId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MachineEngineer", x => x.MachineEngineerId);
+                    table.ForeignKey(
+                        name: "FK_MachineEngineer_Engineers_EngineerId",
+                        column: x => x.EngineerId,
+                        principalTable: "Engineers",
+                        principalColumn: "EngineerId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MachineEngineer_Machines_MachineId",
+                        column: x => x.MachineId,
+                        principalTable: "Machines",
+                        principalColumn: "MachineId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MachineLicense",
+                columns: table => new
+                {
+                    MachineLicenseId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    MachineId = table.Column<int>(nullable: false),
+                    LicenseId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MachineLicense", x => x.MachineLicenseId);
+                    table.ForeignKey(
+                        name: "FK_MachineLicense_Licenses_LicenseId",
+                        column: x => x.LicenseId,
+                        principalTable: "Licenses",
+                        principalColumn: "LicenseId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MachineLicense_Machines_MachineId",
+                        column: x => x.MachineId,
+                        principalTable: "Machines",
+                        principalColumn: "MachineId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LicenseEngineer_EngineerId",
+                table: "LicenseEngineer",
+                column: "EngineerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LicenseEngineer_LicenseId",
+                table: "LicenseEngineer",
+                column: "LicenseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MachineEngineer_EngineerId",
+                table: "MachineEngineer",
+                column: "EngineerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MachineEngineer_MachineId",
+                table: "MachineEngineer",
+                column: "MachineId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MachineLicense_LicenseId",
+                table: "MachineLicense",
+                column: "LicenseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MachineLicense_MachineId",
+                table: "MachineLicense",
+                column: "MachineId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LicenseEngineer");
+
+            migrationBuilder.DropTable(
+                name: "MachineEngineer");
+
+            migrationBuilder.DropTable(
+                name: "MachineLicense");
+
+            migrationBuilder.DropTable(
+                name: "Engineers");
+
+            migrationBuilder.DropTable(
+                name: "Licenses");
+
+            migrationBuilder.DropTable(
+                name: "Machines");
+        }
+    }
+}
