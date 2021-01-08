d0a34420c0dec88eca679f36e17f90611d067921
kevinkirkl3y <kevinmkirkley@gmail.com>
2021-01-08 12:43:11 -0800

    add connections from machine to license and engineers.

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -7,7 +7,11 @@ namespace Factory.Models
 {
   public class Machine
   {
-    
+    public Machine()
+    {
+      this.Engineers = new HashSet<MachineEngineer>();
+      this.Licenses = new HashSet<MachineLicense>();
+    }
     public int MachineId { get; set; }
     public string MachineName { get; set; }
     public string SerialNumber { get; set; }
@@ -15,5 +19,6 @@ namespace Factory.Models
     [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyy-MM-dd}")]
     public DateTime InstallDate { get; set; }
     public virtual ICollection<MachineEngineer> Engineers { get; set; }
+    public virtual ICollection<MachineLicense> Licenses { get; set; }
   }
 }
