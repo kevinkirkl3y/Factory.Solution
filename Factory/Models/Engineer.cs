fe2c149a3facb2690931affd91b84166fb8946da
kevinkirkl3y <kevinmkirkley@gmail.com>
2021-01-08 08:49:43 -0800

    add to Engineer model

---
+++ Factory/Models/Engineer.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace Factory.Models
+{
+  public class Engineer
+  {
+    public Engineer()
+    {
+      this.Machines = new HashSet<MachineEngineer>();
+    }
+    public int EngineerId { get; set; }
+    public string EngineerName { get; set; }
+    public virtual ICollection<MachineEngineer> Machines { get; set; }
+  }
+}
